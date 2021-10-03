cbe59b8a6cce792a4ff4da616bd8c210ccd74a37
kokokojo2 <foxgunplay@gmail.com>
2021-10-03 15:12:19 +0300

    changed DB to PostgreSQL

--- Desk2/settings.py
+++ Desk2/settings.py
@@ -61,8 +61,12 @@ WSGI_APPLICATION = 'Desk2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
