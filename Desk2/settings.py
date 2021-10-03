70eece26b3067acb60d119d0598ac42fd305a537
kokokojo2 <foxgunplay@gmail.com>
2021-10-03 15:23:37 +0300

    installed rest_framework app and 3 sample apps

--- Desk2/settings.py
+++ Desk2/settings.py
@@ -22,6 +22,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
+    'user_accounts',
+    'university_structures',
+    'courses',
 ]
 
 MIDDLEWARE = [
