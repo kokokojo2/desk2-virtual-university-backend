b9e298974fbfd4b85219ae2716eaa47f2577414f
kokokojo2 <foxgunplay@gmail.com>
2021-10-05 10:55:12 +0300

    intermediate ProfileType model removed, docstring on UserAccount model added

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -32,6 +32,12 @@ class UserAccountManager(BaseUserManager):
 
 
 class UserAccount(AbstractBaseUser):
+    """
+    This is a user model used for authentication. The username field is email, so it should be unique.
+    WARNING: access to the wrong reverse profile field raises **RelatedObjectDoesNotExist**,
+    use safe **profile** property instead.
+    """
+
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     middle_name = models.CharField(max_length=128)
@@ -61,19 +67,15 @@ class UserAccount(AbstractBaseUser):
         return f'{self.last_name} {self.first_name}'
 
 
-class ProfileType(models.Model):
-    user = models.OneToOneField(UserAccount, related_name='user', on_delete=models.CASCADE)
-
-
 class TeacherProfile(models.Model):
     scientific_degree = models.CharField(max_length=128)
     position = models.CharField(max_length=128)
-    user = models.OneToOneField(ProfileType, related_name='teacher_profile', on_delete=models.CASCADE)
+    user = models.OneToOneField(UserAccount, related_name='teacher_profile', on_delete=models.CASCADE)
 
 
 class StudentProfile(models.Model):
     student_card_id = models.BigIntegerField()
-    user = models.OneToOneField(ProfileType, related_name='student_profile', on_delete=models.CASCADE)
+    user = models.OneToOneField(UserAccount, related_name='student_profile', on_delete=models.CASCADE)
     # TODO: add foreign key to group
 
 
