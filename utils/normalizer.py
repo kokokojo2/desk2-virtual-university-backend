780c538342a1c750c2b9acc74683ec4199be90bc
kokokojo2 <foxgunplay@gmail.com>
2021-10-05 13:51:48 +0300

    normalizer utility class created

---
+++ utils/normalizer.py
@@ -0,0 +1,10 @@
+class Normalizer:
+
+    @staticmethod
+    def _check_type(**kwargs):
+        pass
+        # TODO: implement type checking
+
+    @staticmethod
+    def first_capital(value: str) -> str:
+        return value.capitalize()
