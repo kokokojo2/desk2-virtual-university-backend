c577e0195110aac1f3f65207f7307d58407aa0a1
kokokojo2 <foxgunplay@gmail.com>
2021-10-06 16:36:20 +0300

    Implemented Faculty, Department, Speciality and Group model
    (cherry picked from commit 65a7562e0098d66fda824323fa6fa1485dea8b6b)

--- university_structures/models.py
+++ university_structures/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Faculty(models.Model):
+    title = models.CharField(max_length=128, unique=True, validators=[MinLengthValidator(2)])
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Department(models.Model):
+    title = models.CharField(max_length=128, unique=True, validators=[MinLengthValidator(2)])
+    description = models.TextField(blank=True)
+    faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Speciality(models.Model):
+    title = models.CharField(max_length=128, unique=True, validators=[MinLengthValidator(2)])
+    code = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f'{self.code} {self.title}'
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=5)
+    study_year = models.PositiveSmallIntegerField()
+
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
