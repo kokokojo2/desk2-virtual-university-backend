2cc7ea290d2cfd1978a0fa21bab58b75f6dde038
kokokojo2 <foxgunplay@gmail.com>
2021-10-07 12:06:11 +0300

    AUTH_USER_MODEL specified

--- Desk2/settings.py
+++ Desk2/settings.py
@@ -86,6 +86,7 @@ REST_FRAMEWORK = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'user_accounts.UserAccount'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
