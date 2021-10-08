ae5130348b50f9b1337a669b2839e472be483116
kokokojo2 <foxgunplay@gmail.com>
2021-10-08 12:48:32 +0300

    AuthenticationViewSet added to url patterns

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -1,11 +1,16 @@
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 
-from django.urls import path
+from .views import AuthenticationViewSet
 
+
+auth_router = DefaultRouter()
+auth_router.register(r'user', AuthenticationViewSet, basename='user')
 
 urlpatterns = [
     path('token/obtain/', TokenObtainPairView.as_view(), name='jwt-obtain'),
     path('token/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
 
-
+    path('', include(auth_router.urls)),
 ]
