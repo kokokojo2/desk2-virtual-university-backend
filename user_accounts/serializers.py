cad744cfa5e811655a8193f83fc69b68f50ad975
kokokojo2 <foxgunplay@gmail.com>
2021-10-08 12:44:19 +0300

    UserAccountSerializer now extends NormalizedModelSerializer and normalizes all char fields as a Capitalized text. save method of UserAccountSerializer is overridden to set password in a safe manner. TeacherProfileSerializer and StudentProfileSerializer default serializers created

--- user_accounts/serializers.py
+++ user_accounts/serializers.py
@@ -1,10 +1,34 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import UserAccount
+from .models import UserAccount, TeacherProfile, StudentProfile
+from utils.normalizers import Normalizer
+from utils.serializers import NormalizedModelSerializer
 
 
-class UserAccountSerializer(ModelSerializer):
+class UserAccountSerializer(NormalizedModelSerializer):
     class Meta:
         model = UserAccount
         exclude = ['password']
-        read_only_fields = ['is_active', 'is_admin', 'edited', 'created']
+        read_only_fields = ['is_active', 'is_admin', 'edited', 'created', 'last_login', 'id']
+        normalize_for_type = {str: Normalizer.first_capital}
+
+    def save(self, **kwargs):
+        instance = super().save(**kwargs)
+
+        if 'password' in kwargs:
+            instance.set_password(kwargs['password'])
+            instance.save()
+
+        return instance
+
+
+class TeacherProfileSerializer(ModelSerializer):
+    class Meta:
+        model = TeacherProfile
+        exclude = ['user', 'id']
+
+
+class StudentProfileSerializer(ModelSerializer):
+    class Meta:
+        model = StudentProfile
+        exclude = ['user', 'id']
