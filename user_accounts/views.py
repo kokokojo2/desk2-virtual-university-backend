5e3118d6ff28365b22d4bff50311dbe2d856a5a5
kokokojo2 <foxgunplay@gmail.com>
2021-10-08 12:47:35 +0300

    create action of AuthenticationViewSet implemented, user model and profile model are created in one request

--- user_accounts/views.py
+++ user_accounts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .utils import get_serializer_for_profile
+from .serializers import UserAccountSerializer
+
+
+class AuthenticationViewSet(ViewSet):
+    """
+    Note: ViewSet base class instead of ModelViewSet is used to implement management of user and profile models in one
+    request.
+    """
+
+    def list(self, request):
+        pass
+
+    def create(self, request):
+        user_serializer = UserAccountSerializer(data=request.data)
+        profile_serializer = get_serializer_for_profile(request.data)
+
+        if profile_serializer is None:
+            return Response(
+                {'profile_type': 'Invalid profile type. Should be student or teacher.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if user_serializer.is_valid():
+            user_object = user_serializer.save(password=request.data['password'])
+        else:
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        if profile_serializer.is_valid():
+            profile_serializer.save(user=user_object)
+        else:
+            return Response(profile_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        response_dict = user_serializer.data
+        response_dict.update(profile_serializer.data)
+
+        return Response(response_dict, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
