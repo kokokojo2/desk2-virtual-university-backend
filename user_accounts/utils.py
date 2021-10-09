82a07caff3c1e435c496607ab2579a7fb283a081
kokokojo2 <foxgunplay@gmail.com>
2021-10-09 14:18:42 +0300

    get_serializer_for_profile_obj created to retrieve appropriate serializer based on model class

--- user_accounts/utils.py
+++ user_accounts/utils.py
@@ -1,4 +1,5 @@
 from .serializers import TeacherProfileSerializer, StudentProfileSerializer
+from .models import TeacherProfile, StudentProfile
 
 
 def get_serializer_for_profile(request_data):
@@ -7,3 +8,18 @@ def get_serializer_for_profile(request_data):
 
     if request_data.get('profile_type', '') == 'teacher':
         return TeacherProfileSerializer(data=request_data)
+
+
+def get_serializer_for_profile_obj(obj, request_data=None):
+    _serializer = None
+    if isinstance(obj, StudentProfile):
+        _serializer = StudentProfileSerializer
+
+    if isinstance(obj, TeacherProfile):
+        _serializer = TeacherProfileSerializer
+
+    if request_data and _serializer:
+        return _serializer(obj, request_data)
+
+    if not request_data and _serializer:
+        return _serializer(obj)
