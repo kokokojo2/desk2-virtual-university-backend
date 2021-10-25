9bdd4666ed9d0009617ff0bb1c5a6f09b1560d63
kokokojo2 <foxgunplay@gmail.com>
2021-10-25 15:23:39 +0300

    -get_serializer_for_profile_obj partial serializer option added - serializer_check_save function implemented (just a shortcut for calling is_valid and saving object)

--- user_accounts/utils.py
+++ user_accounts/utils.py
@@ -10,7 +10,7 @@ def get_serializer_for_profile(request_data):
         return TeacherProfileSerializer(data=request_data)
 
 
-def get_serializer_for_profile_obj(obj, request_data=None):
+def get_serializer_for_profile_obj(obj, request_data=None, partial=False):
     _serializer = None
     if isinstance(obj, StudentProfile):
         _serializer = StudentProfileSerializer
@@ -19,7 +19,15 @@ def get_serializer_for_profile_obj(obj, request_data=None):
         _serializer = TeacherProfileSerializer
 
     if request_data and _serializer:
-        return _serializer(obj, request_data)
+        return _serializer(obj, request_data, partial=partial)
 
     if not request_data and _serializer:
-        return _serializer(obj)
+        return _serializer(obj, partial=partial)
+
+
+def serializer_check_save(serializer, raise_exception=False, **kwargs):
+    if serializer.is_valid(raise_exception=raise_exception):
+        return True, serializer.save(**kwargs)
+
+    else:
+        return False, serializer.errors
