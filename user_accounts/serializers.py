2a0a502a3c0c03e1992abf25927788c857697b28
kokokojo2 <foxgunplay@gmail.com>
2021-10-25 18:22:20 +0300

    PasswordSerializer implemented. calling save method updates password of an instance attribute

--- user_accounts/serializers.py
+++ user_accounts/serializers.py
@@ -1,4 +1,6 @@
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.serializers import ModelSerializer, Serializer, ValidationError
+from rest_framework import serializers
 
 from .models import UserAccount, TeacherProfile, StudentProfile
 from utils.normalizers import Normalizer
@@ -32,3 +34,29 @@ class StudentProfileSerializer(ModelSerializer):
     class Meta:
         model = StudentProfile
         exclude = ['user', 'id']
+
+
+class PasswordSerializer(Serializer):
+    """
+    Serializer class used for changing user password.
+    Requires instance keyword attribute into its __init__ method.
+    """
+    new_password = serializers.CharField(required=True)
+    current_password = serializers.CharField(required=True)
+
+    def validate_current_password(self, value):
+        msg_text = 'Does not match with current password.'
+
+        if not self.instance.check_password(self.initial_data['current_password']):
+            raise ValidationError(msg_text)
+
+        return value
+
+    def validate_new_password(self, value):
+        validate_password(value, user=self.instance)
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['new_password'])
+        instance.save()
+
+        return instance
