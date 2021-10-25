586886b28a21067bf826b518ff0d655771fe048f
kokokojo2 <foxgunplay@gmail.com>
2021-10-25 18:22:57 +0300

    path for ChangePasswordView added

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -2,7 +2,7 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
-from .views import AuthenticationViewSet
+from .views import AuthenticationViewSet, ChangePasswordView
 
 
 auth_router = DefaultRouter()
@@ -11,6 +11,8 @@ auth_router.register(r'user', AuthenticationViewSet, basename='user')
 urlpatterns = [
     path('token/obtain/', TokenObtainPairView.as_view(), name='jwt-obtain'),
     path('token/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
+
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
 
     path('', include(auth_router.urls)),
 ]
