aaf65f0281b235d6171fa450a7fa25a4b51c9c7a
kokokojo2 <foxgunplay@gmail.com>
2021-10-27 15:01:28 +0300

    path for SendTokenView added

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -2,8 +2,9 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 
-from .views import AuthenticationViewSet, ChangePasswordView
+from .views import AuthenticationViewSet, ChangePasswordView, CheckTokenView, SendTokenView
 
+app_name = 'user_account'
 
 auth_router = DefaultRouter()
 auth_router.register(r'user', AuthenticationViewSet, basename='user')
@@ -13,6 +14,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='jwt-refresh'),
 
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
-
+    path('check-token/<token_type>/<uid>/', CheckTokenView.as_view(), name='check-token'),
+    path('send-token/<token_type>/', SendTokenView.as_view(), name='send-token'),
     path('', include(auth_router.urls)),
 ]
