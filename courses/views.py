4e2edc2cb7ee9db943f21b86497c3e522d326b99
kokokojo2 <foxgunplay@gmail.com>
2021-11-09 18:53:32 +0200

    import optimization is done, university structures viewSets are removed as they have been factored out to a separate application

--- courses/views.py
+++ courses/views.py
@@ -1,52 +1,39 @@
-from courses.models import Faculty,Department,Course,\
-    CourseMember,Task,Grade,Chapter,Attachment,StudentWork
-from rest_framework import viewsets
-from rest_framework import permissions
-from courses.serializers import FacultySerializer,CourseSerializer,GradeSerializer,\
-    TaskSerializer,AttachmentSerializer,DepartmentSerializer,ChapterSerializer,\
-    CourseMemberSerializer,StudentWorkSerializer
+from rest_framework.viewsets import ModelViewSet
+from courses.models import Course, CourseMember, Task, Grade, Chapter, Attachment, StudentWork
+from courses.serializers import CourseSerializer, GradeSerializer, TaskSerializer, AttachmentSerializer, \
+    ChapterSerializer, CourseMemberSerializer, StudentWorkSerializer
 
 
-class FacultyViewSet(viewsets.ModelViewSet):
-    queryset = Faculty.objects.all()
-    serializer_class = FacultySerializer
-
-
-class CourseViewSet(viewsets.ModelViewSet):
+class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
 
-class DepartmentViewSet(viewsets.ModelViewSet):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
-
-
-class CourseMemberViewSet(viewsets.ModelViewSet):
+class CourseMemberViewSet(ModelViewSet):
     queryset = CourseMember.objects.all()
     serializer_class = CourseMemberSerializer
 
 
-class TaskViewSet(viewsets.ModelViewSet):
+class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
 
-class GradeViewSet(viewsets.ModelViewSet):
+class GradeViewSet(ModelViewSet):
     queryset = Grade.objects.all()
     serializer_class = GradeSerializer
 
 
-class ChapterViewSet(viewsets.ModelViewSet):
+class ChapterViewSet(ModelViewSet):
     queryset = Chapter.objects.all()
     serializer_class = ChapterSerializer
 
-class AttachmentViewSet(viewsets.ModelViewSet):
+
+class AttachmentViewSet(ModelViewSet):
     queryset = Attachment.objects.all()
     serializer_class = AttachmentSerializer
 
 
-class StudentWorkViewSet(viewsets.ModelViewSet):
+class StudentWorkViewSet(ModelViewSet):
     queryset = StudentWork.objects.all()
     serializer_class = StudentWorkSerializer
-
