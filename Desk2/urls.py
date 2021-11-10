17b925c8afd2952b372cf9888d95fa7e38353af8
kokokojo2 <foxgunplay@gmail.com>
2021-11-10 11:02:30 +0200

    url patterns restructured, courses app urls were factored out into courses/urls.py

--- Desk2/urls.py
+++ Desk2/urls.py
@@ -1,26 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import path, include
-from courses.views import CourseViewSet,GradeViewSet,TaskViewSet,\
-    ChapterViewSet,CourseMemberViewSet,\
-    AttachmentViewSet,StudentWorkViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-#router.register(r'courses', CourseViewSet)
-#router.register(r'users', UserViewSet)
-#router.register(r'groups', GroupViewSet)
-router.register('courses', CourseViewSet)
-router.register('course_members', CourseMemberViewSet)
-router.register('tasks', TaskViewSet)
-router.register('chapters', ChapterViewSet)
-router.register('grades', GradeViewSet)
-router.register('attachments', AttachmentViewSet)
-router.register('student_works', StudentWorkViewSet)
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('user_accounts.urls')),
-    path('api/', include(router.urls)),
+    path('api/auth/', include('user_accounts.urls')),
+    path('api/', include('courses.urls')),
 ]
---
+++ courses/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from courses.views import CourseViewSet, GradeViewSet, TaskViewSet, ChapterViewSet, CourseMemberViewSet, \
+    AttachmentViewSet, StudentWorkViewSet
+
+display_per_course_router = DefaultRouter()
+display_per_course_router.register('course_members', CourseMemberViewSet)
+display_per_course_router.register('chapters', ChapterViewSet)
+display_per_course_router.register('tasks', TaskViewSet)
+display_per_course_router.register('student_works', StudentWorkViewSet)
+display_per_course_router.register('grades', GradeViewSet)
+display_per_course_router.register('attachments', AttachmentViewSet)
+
+courses_router = DefaultRouter()
+courses_router.register('courses', CourseViewSet)
+
+urlpatterns = [
+    path('', include(courses_router.urls)),
+    path('<int:course_id>/', include(display_per_course_router.urls)),
+]
