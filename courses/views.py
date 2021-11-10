2eee103f210c7e15beb9df4ec48417ddef6dab24
kokokojo2 <foxgunplay@gmail.com>
2021-11-10 15:03:37 +0200

    CourseViewSet implemented

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
 from courses.models import Course, CourseMember, Task, Grade, Chapter, Attachment, StudentWork
 from courses.serializers import CourseSerializer, GradeSerializer, TaskSerializer, AttachmentSerializer, \
     ChapterSerializer, CourseMemberSerializer, StudentWorkSerializer
+from .permissions import IsOwnerOrReadOnly, IsGlobalTeacherOrReadOnly
 
 
 class CourseViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated, IsGlobalTeacherOrReadOnly, IsOwnerOrReadOnly]
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        if self.request.GET.get('enrolled', False):
+            return self.queryset.filter(coursemember__in=self.request.user.get_course_members_queryset())
+
+        return self.queryset
 
 
 class CourseMemberViewSet(ModelViewSet):
