f76ed35d5a3c519004bf8f8211f69ded8aa325df
kokokojo2 <foxgunplay@gmail.com>
2021-11-12 23:52:39 +0200

    CourseMiddleware class is added to MIDDLEWARE settings

--- Desk2/settings.py
+++ Desk2/settings.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'courses.middleware.CourseMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
