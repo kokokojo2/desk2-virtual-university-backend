575ba38d9cf3d1d5b2e0871bab2ba57785e49fd3
kokokojo2 <foxgunplay@gmail.com>
2021-11-15 16:12:15 +0200

    IsOwnerOrReadOnly permission is deleted, BaseIsOwnerOrAllowMethods permission should be used instead

--- courses/permissions.py
+++ courses/permissions.py
@@ -23,31 +23,6 @@ class BaseIsOwnerOrAllowMethods(BasePermission):
         raise AttributeError('The specified owner field does not exist on a given model.')
 
 
-class IsOwnerOrReadOnly(BasePermission):
-    """
-    Grants permission to modify the object if the request.user is the owner of a object.
-    Grants read-only permission for every request.
-
-    Uses generic mechanism to get the owner object.
-    """
-    message = 'You are not the owner.'
-    default_owner_lookup_fields = ('owner', 'author')
-    custom_owner_lookup_fields = ()  # override this field to provide custom owner lookup fields
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-
-        for potential_owner_field in chain(self.default_owner_lookup_fields, self.custom_owner_lookup_fields):
-            owner = getattr(obj, potential_owner_field, None)
-            if owner:
-                return owner == request.user
-
-        raise AttributeError('Permission checker has not found the owner field of a given object. Make sure an '
-                             'appropriate model has an owner field with the name from one of default_owner_lookup_fields'
-                             ' or provide a custom owner field name by overriding custom_owner_lookup_fields.')
-
-
 class IsGlobalTeacher(BasePermission):
     """
     Grants permission to a request if request.user is a teacher (has a teacher profile type)
