30b8dd062747552ecc79c60cde5b52948e957580
kokokojo2 <foxgunplay@gmail.com>
2021-11-18 15:27:44 +0200

    the field to get the url address for patterns with the course_id parameter was created

---
+++ courses/serializer_fields.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import HyperlinkedIdentityField
+from rest_framework.reverse import reverse
+
+
+class CourseRelatedHyperlinkedIdentityField(HyperlinkedIdentityField):
+
+    def get_url(self, obj, view_name, request, format):
+        url_kwargs = {
+            'course_id': request.resolver_match.kwargs['course_id'],
+            'pk': obj.pk
+        }
+
+        return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
