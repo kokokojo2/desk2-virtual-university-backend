29d705c0a1e43745101bde41cade6a45361ebcf4
kokokojo2 <foxgunplay@gmail.com>
2021-11-19 15:50:50 +0200

    the get_queryset now filters material and task sets to return only appropriate posts depending on a user type

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,12 @@
+from django.db.models import Prefetch
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from rest_framework import mixins
+from django.utils import timezone
 
 from courses.models import Course, CourseMember, Task, Grade, Chapter, Attachment, StudentWork, Material
 from courses.serializers import CourseSerializer, GradeSerializer, TaskSerializer, AttachmentSerializer, \
-    ChapterRestrictedSerializer, ChapterUnrestrictedSerializer, CourseMemberSerializer, StudentWorkSerializer,\
+    ChapterSerializer, CourseMemberSerializer, StudentWorkSerializer,\
     MaterialSerializer
 from .permissions import IsGlobalTeacherOrReadOnly, BaseIsOwnerOrAllowMethods,\
     BaseIsTeacherOrAllowMethods
@@ -58,10 +60,19 @@ class MaterialViewSet(ModelViewSet):
     queryset = Material.objects.all()
     serializer_class = MaterialSerializer
 
+    def get_queryset(self):
+        return self.queryset.filter(chapter__in=self.request.course.chapter_set.all())
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.course_member)
+
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.course_member)
 
 
 class GradeViewSet(ModelViewSet):
@@ -74,19 +85,28 @@ class ChapterViewSet(ModelViewSet):
         allow_methods = SAFE_METHODS
 
     permission_classes = [IsAuthenticated, IsTeacherOrReadOnly]
+    serializer_class = ChapterSerializer
     queryset = Chapter.objects.all()
 
     def get_queryset(self):
-        return self.queryset.filter(course=self.request.course).prefetch_related('material_set')
+        queryset = self.queryset.filter(course=self.request.course)
+        if not self.request.course_member.is_teacher:
+            return queryset.prefetch_related(
+                Prefetch('material_set', queryset=Material.objects.filter(
+                    is_archived=False,
+                    published_at__lte=timezone.now()
+                ))
+            ).prefetch_related(
+                Prefetch('task_set', queryset=Task.objects.filter(
+                    is_archived=False,
+                    published_at__lte=timezone.now()
+                ))
+            )
+
+        return queryset.prefetch_related('material_set').prefetch_related('task_set')
 
     def perform_create(self, serializer):
         serializer.save(course=self.request.course)
-
-    def get_serializer_class(self):
-        if self.request.course_member.is_teacher:
-            return ChapterUnrestrictedSerializer
-
-        return ChapterRestrictedSerializer
 
 
 class AttachmentViewSet(ModelViewSet):
