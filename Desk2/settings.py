6cdeb8fd6239a8e0e05b8a01888c78e28bcc42c8
kokokojo2 <foxgunplay@gmail.com>
2021-11-19 19:26:57 +0200

    'attachments/' folder added to STATICFILES_DIRS

--- Desk2/settings.py
+++ Desk2/settings.py
@@ -133,7 +133,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'attachments/'
+]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
