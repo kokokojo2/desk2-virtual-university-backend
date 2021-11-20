6e184df570ebc8263f1fd711c71c39d8d5ea6404
kokokojo2 <foxgunplay@gmail.com>
2021-11-20 18:03:06 +0200

    added new ChapterRelatedHyperlinkedIdentityField to reverse urls with chapter_id parameter

--- courses/serializer_fields.py
+++ courses/serializer_fields.py
@@ -11,3 +11,14 @@ class CourseRelatedHyperlinkedIdentityField(HyperlinkedIdentityField):
         }
 
         return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
+
+
+class ChapterRelatedHyperlinkedIdentityField(HyperlinkedIdentityField):
+    def get_url(self, obj, view_name, request, format):
+        url_kwargs = {
+            'course_id': request.resolver_match.kwargs['course_id'],
+            'chapter_id': obj.chapter.pk,
+            'pk': obj.pk
+        }
+
+        return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
