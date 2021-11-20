98704f3ed15f4531789d03dfa58956dcd0b7d404
kokokojo2 <foxgunplay@gmail.com>
2021-11-20 18:01:21 +0200

    material and task urls now are accessible only by chapter

--- courses/urls.py
+++ courses/urls.py
@@ -5,13 +5,18 @@ from courses.views import CourseViewSet, GradeViewSet, TaskViewSet, ChapterViewS
     AttachmentViewSet, StudentWorkViewSet, MaterialViewSet
 
 display_per_course_router = DefaultRouter()
+
 display_per_course_router.register('course_members', CourseMemberViewSet)
 display_per_course_router.register('chapters', ChapterViewSet)
-display_per_course_router.register('materials', MaterialViewSet)
-display_per_course_router.register('tasks', TaskViewSet)
 display_per_course_router.register('student_works', StudentWorkViewSet)
 display_per_course_router.register('grades', GradeViewSet)
 display_per_course_router.register('attachments', AttachmentViewSet)
+
+display_per_chapter_router = DefaultRouter()
+
+display_per_chapter_router.register('materials', MaterialViewSet)
+display_per_chapter_router.register('tasks', TaskViewSet)
+
 
 courses_router = DefaultRouter()
 courses_router.register('courses', CourseViewSet)
@@ -19,4 +24,5 @@ courses_router.register('courses', CourseViewSet)
 urlpatterns = [
     path('', include(courses_router.urls)),
     path('<int:course_id>/', include(display_per_course_router.urls)),
+    path('<int:course_id>/<int:chapter_id>/', include(display_per_chapter_router.urls))
 ]
