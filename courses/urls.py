c61eee2bf75adabe41e3c2b1192e827203854a4b
kokokojo2 <foxgunplay@gmail.com>
2021-11-23 13:12:23 +0200

    student works now can be managed only per task

--- courses/urls.py
+++ courses/urls.py
@@ -8,7 +8,6 @@ display_per_course_router = DefaultRouter()
 
 display_per_course_router.register('course_members', CourseMemberViewSet)
 display_per_course_router.register('chapters', ChapterViewSet)
-display_per_course_router.register('student_works', StudentWorkViewSet)
 display_per_course_router.register('grades', GradeViewSet)
 display_per_course_router.register('attachments', AttachmentViewSet)
 
@@ -17,6 +16,8 @@ display_per_chapter_router = DefaultRouter()
 display_per_chapter_router.register('materials', MaterialViewSet)
 display_per_chapter_router.register('tasks', TaskViewSet)
 
+student_work_router = DefaultRouter()
+student_work_router.register('student-works', StudentWorkViewSet)
 
 courses_router = DefaultRouter()
 courses_router.register('courses', CourseViewSet)
@@ -24,5 +25,6 @@ courses_router.register('courses', CourseViewSet)
 urlpatterns = [
     path('', include(courses_router.urls)),
     path('<int:course_id>/', include(display_per_course_router.urls)),
-    path('<int:course_id>/<int:chapter_id>/', include(display_per_chapter_router.urls))
+    path('<int:course_id>/<int:chapter_id>/', include(display_per_chapter_router.urls)),
+    path('<int:course_id>/<int:chapter_id>/tasks/<int:task_id>/', include(student_work_router.urls)),
 ]
