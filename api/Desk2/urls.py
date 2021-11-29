90145ed77c02f2ad25d8e1258d62d265cec9cb4a
kokokojo2 <foxgunplay@gmail.com>
2021-11-30 00:34:36 +0200

    urls from university_structures app were included

--- api/Desk2/urls.py
+++ api/Desk2/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('user_accounts.urls')),
+    path('api/structures/', include('university_structures.urls')),
     path('api/', include('courses.urls')),
 ]
