433e89243405767afe022920dd117f02cee318d1
kokokojo2 <foxgunplay@gmail.com>
2021-11-30 00:16:21 +0200

    ReadOnlyViewSets for university_structures models were created

--- api/university_structures/views.py
+++ api/university_structures/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from .models import Degree, Department, Speciality, Position, Faculty, Group
+from .serializers import DegreeSerializer, PositionSerializer, SpecialitySerializer, FacultySerializer, \
+    DepartmentSerializer, GroupSerializer
+
+
+class DegreeViewSet(ReadOnlyModelViewSet):
+    queryset = Degree.objects.all()
+    serializer_class = DegreeSerializer
+
+
+class PositionViewSet(ReadOnlyModelViewSet):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
+
+
+class SpecialityViewSet(ReadOnlyModelViewSet):
+    queryset = Speciality.objects.all()
+    serializer_class = SpecialitySerializer
+
+
+class FacultyViewSet(ReadOnlyModelViewSet):
+    queryset = Faculty.objects.all()
+    serializer_class = FacultySerializer
+
+
+class DepartmentViewSet(ReadOnlyModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class GroupViewSet(ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
