80672a5cb01a55b190407813289c8c6e5f8a6962
kokokojo2 <foxgunplay@gmail.com>
2021-12-01 21:59:53 +0200

    Added default MaterialAdmin and ChapterAdmin

--- api/courses/admin.py
+++ api/courses/admin.py
@@ -16,4 +16,20 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ['owner__first_name', 'owner__last_name', 'title']
 
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published_at', 'chapter', 'is_archived']
+    readonly_fields = ['is_planned', 'created_at', 'edited_at']
+    list_filter = ['is_archived', 'author__user']
+
+
+class MaterialAdmin(PostAdmin):
+    model = models.Material
+
+
+class ChapterAdmin(admin.ModelAdmin):
+    model = models.Chapter
+
+
 admin.site.register(models.Course, CourseAdmin)
+admin.site.register(models.Material, MaterialAdmin)
+admin.site.register(models.Chapter, ChapterAdmin)
