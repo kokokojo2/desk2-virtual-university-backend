d097b4cc10b869bdbab48ee41fc6ff60ab82a5d5
kokokojo2 <foxgunplay@gmail.com>
2021-12-01 15:10:22 +0200

    UserAccount was registered in the admin and modelAdmin was configured, inlineAdmins for associated profile objects were configured

--- api/user_accounts/admin.py
+++ api/user_accounts/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import UserAccount, TeacherProfile, StudentProfile
 
-# Register your models here.
+
+class StudentProfileModelInline(admin.TabularInline):
+    model = StudentProfile
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class TeacherProfileModelInline(admin.TabularInline):
+    model = TeacherProfile
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class UserAccountAdmin(admin.ModelAdmin):
+    model = UserAccount
+    # list page
+    list_display = ['first_name', 'last_name', 'email', 'department', 'registration_type', 'is_active']
+    list_filter = ['department', 'is_active', 'registration_type']
+    search_fields = ['first_name', 'last_name', 'email']
+
+    # detail page
+    readonly_fields = ['last_login', 'registration_type']
+    exclude = ['password']
+
+    def get_inlines(self, request, obj):
+        try:
+            profile_instance = obj.profile
+            if isinstance(profile_instance, StudentProfile):
+                return [StudentProfileModelInline]
+
+            return [TeacherProfileModelInline]
+        except AttributeError:
+            return []
+
+
+admin.site.register(UserAccount, UserAccountAdmin)
