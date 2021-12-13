1c24c34a1956cac018f893830b7da175ee362c46
kokokojo2 <foxgunplay@gmail.com>
2021-12-13 14:33:19 +0200

    IsGlobalTeacherTestCase implemented

--- api/tests/courses/unit/tests.py
+++ api/tests/courses/unit/tests.py
@@ -1,9 +1,28 @@
-from django.test import TestCase
-from tests import utils
+from django.test import TestCase, RequestFactory
+from django.urls import resolve
+from tests import utility_funcs
+from courses.permissions import *
 
 
 class PermissionTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.student, cls.teacher = utils.populate_users()
-        cls.course = utils.populate_course(cls.teacher, cls.student)
+        cls.teacher, cls.student = utility_funcs.populate_users()
+        cls.course = utility_funcs.populate_course(cls.teacher, [cls.student])
+
+
+class IsGlobalTeacherTestCase(PermissionTestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.permission = IsGlobalTeacher()
+
+    def test_success(self):
+        request = self.factory.get('api/courses/1/')
+        setattr(request, 'user', self.teacher)
+        self.assertEquals(True, self.permission.has_permission(request, None))
+
+    def test_fail(self):
+        request = self.factory.get('api/courses/1/')
+        setattr(request, 'user', self.student)
+        self.assertEquals(False, self.permission.has_permission(request, None))
