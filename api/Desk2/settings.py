7a13ac0b84b9a932213f0eadbf24fc2067a7a52d
kokokojo2 <foxgunplay@gmail.com>
2021-12-13 20:42:58 +0200

    smtp server configured

--- api/Desk2/settings.py
+++ api/Desk2/settings.py
@@ -142,8 +142,14 @@ STATICFILES_DIRS = [
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
+# uncommit for testing
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # auth app settings
 REDIS_STORED_TOKENS = True
